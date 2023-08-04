64f8e78e002dcf6ef91dd055488f7ea59d234989
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-04 14:00:35 +0900

    プロトタイプ投稿機能４

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,4 +5,15 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
   end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
+  end
+
 end
