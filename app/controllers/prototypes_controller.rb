65531918d2e471a40b9bbb102e3a7842442d318e
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-04 17:45:04 +0900

    プロトタイプ詳細機能2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -18,6 +18,11 @@ class PrototypesController < ApplicationController
       end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
